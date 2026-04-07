78f375f81e76954acacc8c05d587c7ac0fdc7239
syfaarizal <syifairgi@gmail.com>
2026-04-07 13:30:48 +0700

    add intent MessageContent + register event messageCreate

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 require("dotenv").config()
 
 const { Client, GatewayIntentBits } = require("discord.js")
-const { onReady }             = require("./events/ready")
+const { onReady }           = require("./events/ready")
 const { onInteractionCreate } = require("./events/interactionCreate")
+const { onMessageCreate }   = require("./events/messageCreate")
 
 const requiredEnvs = ["TOKEN", "AI_KEY", "CLIENT_ID"]
 for (const key of requiredEnvs) {
@@ -16,11 +17,13 @@ const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-  ]
+    GatewayIntentBits.MessageContent, 
+  ],
 })
 
-client.once("clientReady", () => onReady(client))
-client.on("interactionCreate", (i) => onInteractionCreate(i))
+client.once("clientReady",      () => onReady(client))
+client.on("interactionCreate",  (i) => onInteractionCreate(i))
+client.on("messageCreate",      (m) => onMessageCreate(m))
 
 process.on("unhandledRejection", err => console.error("⚠️ Unhandled Rejection:", err))
 process.on("uncaughtException",  err => console.error("💥 Uncaught Exception:",  err))
